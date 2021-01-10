04744139adf3c9b6232189f9856bbb3ce8e24926
MR404cyber <75874865+MR404cyber@users.noreply.github.com>
2021-01-10 23:22:23 +0800

    Update and rename README.md to hackwa.sh

---
+++ hackwa.sh
@@ -0,0 +1,57 @@
+#! bin / sh
+bgp = ' \ 033 [0; 107m '
+r = ' \ 033 [1; 31m '
+h = ' \ 033 [1; 32m '
+k = ' \ 033 [1; 33m '
+b = ' \ 033 [1; 34m '
+u = ' \ 033 [1; 35m '
+c = ' \ 033 [1; 36m '
+p = ' \ 033 [1; 37m '
+jelas = ' \ 033 [0m '
+ku = ' \ 033 [33; 1m '  # kuning
+bi = ' \ 033 [34; 1m '  # biru
+i = ' \ 033 [32; 1m '  # ijo
+pur = ' \ 033 [35; 1m '  # ungu
+cy = ' \ 033 [36; 1m '  # cyan
+me = ' \ 033 [31; 1m '  # merah
+pu = ' \ 033 [37; 1m '  # putih
+ku = ' \ 033 [33; 1m '  # kuning
+mer = ' \ 033 [41; 97m '  # Tepi
+R = ' \ x1b [1; 31m '
+G = ' \ x1b [1; 32m '
+B = ' \ x1b [1; 34m '
+Y = ' \ x1b [1; 33m '
+C = ' \ x1b [1; 36m '
+D = ' \ x1b [0m '
+endc = ' \ E [0m '
+enda = ' \ 033 [0m '
+bersih
+figlet -f pagga " Login Dulu "  | lolcat -a -d 5
+
+echo  $ saya " ╔══════════╗ "
+echo  $ ku " ║ Kata sandi: "
+echo  $ pu " ╚══════════╝ "
+baca  pwd ;
+
+jika [ $ pwd  =  " MR.404 " ]
+kemudian
+   echo  " \ 033 [1; 32mPasswordnya Bener Bhujank: v "
+tidur 2
+lain
+   echo  " \ 033 [1; 31mPasswordnya Salah bego! "
+   echo  " \ 033 [1; 31mCoba Chat Mr.404 "
+tidur 3
+xdg-buka ' https://wa.me/6282331072836/?text=Assalamualaikum+Apa+passnya+MR.404 '
+   echo  " \ 033 [1; 35m "
+keluar
+fi
+echo  " $ {c} Buatan: MR.404 "
+echo  " $ {b} youtube: MR.404 "
+echo  " $ {p} pesan $ {r} : $ {k} TOOLS HACK WA !!! "
+echo  " $ {u} "
+baca -p " [?] Enter To Continue "  lanjutkan
+bersih
+echo  " $ {p} # $ {r}  $ {k} hack via no hp(62) "
+read -p " [?] masukkan no hp "  terus
+echo  " $ {u} DATA HP TERKENA TROJAN "
+baca -p " tapi boong "  lanjutkan
